f8241fc34e9c31697bb221912f669a30ae9aae6c
avdosev <avdosev1@yandex.ru>
2021-08-01 13:28:00 +0300

    init empty

---
+++ lib/itertools.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library itertools;
+
+// export 'src/itertools_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
